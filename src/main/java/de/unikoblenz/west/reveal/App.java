1d8d3806c5d1d3fd25a9647ecda7884cd286a582
Jun Sun <yfiua@qq.com>
2016-09-22 13:29:44 +0200

    mongoDB works!

--- src/main/java/de/unikoblenz/west/reveal/App.java
+++ src/main/java/de/unikoblenz/west/reveal/App.java
@@ -1,13 +1,16 @@
 package de.unikoblenz.west.reveal;
 
-/**
- * Hello world!
- *
- */
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	MongoClient mongoClient = new MongoClient("social1.atc.gr");
+    	MongoDatabase db = mongoClient.getDatabase("us_elections");
+    	
+        System.out.println(db.getCollection("Post").count());
+        mongoClient.close();
     }
 }
