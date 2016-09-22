f71ba25c5abd2430f3fbe85ae1b50b8b03ee717f
Jun Sun <yfiua@qq.com>
2016-09-22 13:08:16 +0200

    using maven

---
+++ src/main/java/de/unikoblenz/west/reveal/App.java
@@ -0,0 +1,13 @@
+package de.unikoblenz.west.reveal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
