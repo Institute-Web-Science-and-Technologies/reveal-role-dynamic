fa6ef9f6c9ea695391bf1dc5a611b51565b468bf
Jun Sun <yfiua@qq.com>
2016-09-22 13:33:46 +0200

    import important stuffs

--- src/main/java/de/unikoblenz/west/reveal/App.java
+++ src/main/java/de/unikoblenz/west/reveal/App.java
@@ -1,7 +1,14 @@
 package de.unikoblenz.west.reveal;
 
+import org.bson.Document;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.Block;
+import com.mongodb.client.FindIterable;
+
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Sorts.ascending;
+import static java.util.Arrays.asList;
 
 public class App 
 {
@@ -10,7 +17,7 @@ public class App
     	MongoClient mongoClient = new MongoClient("social1.atc.gr");
     	MongoDatabase db = mongoClient.getDatabase("us_elections");
     	
-        System.out.println(db.getCollection("Post").count());
+        System.out.println("Post number : " + db.getCollection("Post").count());
         mongoClient.close();
     }
 }
