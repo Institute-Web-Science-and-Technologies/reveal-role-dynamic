2b9c2dffaf2eb50a8953c2c5ab0d4b43f4e3d9f1
Jun Sun <yfiua@qq.com>
2016-09-23 11:03:25 +0200

    correct parsing to JSON

--- src/main/java/de/unikoblenz/west/reveal/App.java
+++ src/main/java/de/unikoblenz/west/reveal/App.java
@@ -12,6 +12,14 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class App {
+	
+	private static String limitLongStringLength(String s) {
+		if (s.length() > 18)
+			return s.substring(s.length() - 18);
+		
+		return s;
+	}
+	
     public static void main( String[] args ) throws FileNotFoundException {
     	try ( PrintWriter outputFile = new PrintWriter("mongo-data.txt") ) {
 	    	MongoClient mongoClient = new MongoClient("social1.atc.gr");
@@ -22,36 +30,60 @@ public class App {
 
 	        iterable.forEach(new Block<Document>() {
 	            public void apply(final Document document) {
-	            	String id = document.get("_id").toString().split("#")[1];
+	            	String strId = document.get("_id").toString().split("#")[1].replaceAll("[^0-9]", "");
 	            	String title = document.get("title").toString();
 	            	String contributor = document.get("contributor").toString();
+	            	
 	            	String shared = null;
 	            	if (document.get("shared") != null)
 	            		shared = document.get("shared").toString();
 	            	
 	            	// parsing data
 	            	try {
-		            	String contributorId = contributor.split("id\" : \"| }")[1];
-		            	String sharedId = null;
+		            	String strContributorId = contributor.split("#| }")[1].replaceAll("[^0-9]", "");
+		            	String strSharedId = "0";
 		            	if (shared != null)
-		            		sharedId = shared.split("id\" : \"| }")[1];
+		            		strSharedId = shared.split("#| }")[1].replaceAll("[^0-9]", "");
+		            	
+		            	// cast String to Long
+		            	Long id = 0L;
+		            	try {
+		            		strId = limitLongStringLength(strId);
+		            		id = Long.parseLong(strId);
+		            	} catch (NumberFormatException ex) {
+		            		System.err.println("Invalid ID, " + ex.getMessage());
+		            	}
+		            	Long contributorId = 0L;
+		            	try {
+		            		strContributorId = limitLongStringLength(strContributorId);
+		            		contributorId = Long.parseLong(strContributorId);
+		            	} catch (NumberFormatException ex) {
+		            		System.err.println("Invalid ID, " + ex.getMessage());
+		            	}
+		            	Long sharedId = 0L;
+		            	try {
+		            		strSharedId = limitLongStringLength(strSharedId);
+		            		sharedId = Long.parseLong(strSharedId);
+		            	} catch (NumberFormatException ex) {
+		            		System.err.println("Invalid ID, " + ex.getMessage());
+		            	}
 		            	
 		            	JSONObject json = new JSONObject();
 		            	JSONObject user = new JSONObject();
 		            	
 		            	user.put("id", contributorId);
-
+		            	user.put("name", strContributorId);
+		            	
 		            	json.put("id", id);
 		            	json.put("user", user);
 		            	json.put("text", title);
-		            	if (sharedId != null)
+		            	if (shared != null)
 		            		json.put("in_reply_to_user_id", sharedId);
 		    
 		                outputFile.println(json.toString());
 		     
-	            	} catch(ArrayIndexOutOfBoundsException ex) {
-	            		System.out.println(contributor);
-	            		System.err.println(ex.getMessage());
+	            	} catch (ArrayIndexOutOfBoundsException ex) {
+	            		System.err.println("Array index out of bounds, " + ex.getMessage());
 	            	}
 	            }
 	        });
